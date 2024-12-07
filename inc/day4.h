7ebcc5d2617ae279a84b7bc714e41e0cd424437f
liv-codes-stuff <liv@liv-codes-stuff>
2024-12-07 12:38:47 +0100

    Day 4 done

---
+++ inc/day4.h
@@ -0,0 +1,13 @@
+/*
+ * day4.h
+ *
+ *  Created on: 06.12.2024
+ *      Author: liv
+ */
+
+#ifndef DAY4_H_
+#define DAY4_H_
+
+void day4();
+
+#endif /* DAY4_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "day1.h"
 #include "day2.h"
 #include "day3.h"
+#include "day4.h"
 
 int main()
 {
 //	day1();
 //	day2();
-	day3();
+//	day3();
+	day4();
 }
