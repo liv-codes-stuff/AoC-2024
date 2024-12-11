bc97c8e3ff318bcaafae19fe0c6ae7359e8a545a
liv-codes-stuff <liv@liv-codes-stuff>
2024-12-12 00:05:18 +0100

    day 9 and 11

---
+++ inc/day11.h
@@ -0,0 +1,13 @@
+/*
+ * day11.h
+ *
+ *  Created on: 11.12.2024
+ *      Author: liv
+ */
+
+#ifndef DAY11_H_
+#define DAY11_H_
+
+void day11();
+
+#endif /* DAY11_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,21 @@
 #include "day6.h"
 #include "day7.h"
 
+#include "day9.h"
+
+#include "day11.h"
+
 int main()
 {
 //	day1();
 //	day2();
 //	day3();
 //	day4();
-	day5();
+//	day5();
 //	day6();
 //	day7();
+
+//	day9();
+
+	day11();
 }
